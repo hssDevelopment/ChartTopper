bc24c53c4b15414c40188f0158469205d6d5b68d
Hensley, Michael (DS) <Michael.Hensley@cdk.com>
2015-07-08 16:34:56 -0700

    Created ChartTopper service in angular that can draw a basic bar chart and line chart with default width based on information from Ben Sullins course on pluralsite on D3 visualization - Bar chart has fluid api - line chart does not

---
+++ src/app/components/ChartTopper/ChartTopper.js
@@ -0,0 +1,121 @@
+/**
+ * Created by hensleym on 7/8/15.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('chartTopperDemo')
+        .service('chartTopper', chartTopper);
+
+    /** @ngInject */
+    function chartTopper($log, $window) {
+
+        var WIDTH = 150;
+        var HEIGHT = 200;
+        var PADDING = 2;
+
+        function buildBarChart(data, element) {
+            $log.info('element: ' + element);
+            $log.info('data:' + data);
+
+            var svg = create(element);
+            createBars(svg, data);
+            addLabels(svg, data);
+        }
+
+        function buildLineChart(data, element){
+            var lineFunction = d3.svg.line()
+                .x(function(d) {return d.month * 3; })
+                .y(function(d) {return HEIGHT - d.sales; })
+                .interpolate("linear");
+
+            var svg = d3.select(element)
+                .append("svg")
+                .attr({
+                    width: WIDTH,
+                    height: HEIGHT
+                });
+
+            var viz = svg.append("path")
+                .attr({
+                    d: lineFunction(data),
+                    "stroke": "purple",
+                    "stroke-width": 2,
+                    "fill": "none"
+                });
+
+            var labels = svg.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .text(function(d){return d.sales})
+                .attr({
+                    x: function(d){ return d.month * 3 - 25},
+                    y: function(d){ return HEIGHT - d.sales},
+                    "font-size": "12px",
+                    "font-family": "sans-serif",
+                    "fill" : "#666666",
+                    "text-anchor": "start",
+                    "dy": ".35em",
+                    "font-weight": "bold"
+                })
+        }
+
+        var public_api = {
+            buildBarChart: buildBarChart,
+            buildLineChart: buildLineChart
+        };
+
+        return public_api;
+
+        function create(element){
+            var svg = $window.d3.select(element)
+                .append("svg")
+                .attr("width", WIDTH)
+                .attr("height", HEIGHT);
+            return svg;
+        }
+
+        function createBars(svg, data){
+            svg.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr({
+                    x: function (d, i) {
+                        return i * (WIDTH / data.length);
+                    },
+                    y: function (d) {
+                        return HEIGHT - (d * 4);
+                    },
+                    width: WIDTH / data.length - PADDING,
+                    height: function (d) {
+                        return d * 4;
+                    },
+                    fill: function (d) {
+                        return "rgb(0," + (d * 10) + ", 0)";
+                    }
+                });
+        }
+
+        function addLabels(svg, data){
+            svg.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .text(function(d) {return d;})
+                .attr({
+                    "text-anchor" : "middle",
+                    "x": function(d, i){
+                        return i * (WIDTH / data.length) + (WIDTH / data.length - PADDING) / 2 ;
+                    },
+                    "y": function(d) {return HEIGHT - (d * 4) + 15},
+                    "font-family": "sans-serif",
+                    "font-size": 12,
+                    "fill": "#ffffff"
+                });
+        }
+
+    }
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,29 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('chartTopperDemo')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController(chartTopper) {
+      var data = [5, 10, 15, 20, 25];
+      var lineData = [
+          {"month": 10, "sales": 20},
+          {"month": 20, "sales": 100},
+          {"month": 30, "sales": 150},
+          {"month": 40, "sales": 121},
+          {"month": 50, "sales": 80},
+          {"month": 60, "sales": 65},
+          {"month": 70, "sales": 48},
+          {"month": 80, "sales":55},
+          {"month": 90, "sales": 23},
+          {"month": 100, "sales": 7}
+      ];
+      var element = '#d3-test';
+      var lineElement = '#d3-line-test';
+
+      chartTopper.buildBarChart(data, element);
+      chartTopper.buildLineChart(lineData, lineElement);
+  }
+})();
